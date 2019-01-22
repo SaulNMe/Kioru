138c6cfb440b4f6f9d2551d8fbd3c71a5ebe6f11
SaulNMe <40306827+SaulNMe@users.noreply.github.com>
2019-01-22 16:49:42 -0600

    Updated responsive

--- Kioru Website/js/main.js
+++ Kioru Website/js/main.js
@@ -1,14 +1,16 @@
 $(function () {
 	
-
+	/*Auto start function*/
 	(function( window) {
 		resize();
 	})(window);
 
-	
+	/*Function for start resizing function*/
 	$(window).resize(function(){
 	  	resize();
 	});
+
+	/*Function for adding and deleting clases if an element in HTMl*/
 	function handleClass(element, clases, add){
 		var idElement = document.getElementById(element);
 		$.each(clases, function(i, val){
@@ -20,12 +22,7 @@ $(function () {
 			}
 		})
 	}
-	/*function addItemsToVar(elements){
-		$.each(elements, function(i, val){
-			removeItems.add(val);
-			console.log(removeItems);
-		})
-	}*/
+	/*Resize function when window is gotten bigger or smaller*/
 	function resize(){
 		let servicesContainer = document.getElementById("services-container");
 		let servicesContainer2 = document.getElementById("services-container2");
@@ -33,32 +30,82 @@ $(function () {
 		let removeWidth2 = document.getElementById("remove-width2");
 		var removeItems = [];
 		var addItems = [];
+		let aboutUs = document.getElementById("about-us");
+		let misionVision = document.getElementById("mision-vision");
+		let history = document.getElementById("history");
+		let awards = document.getElementById("awards");
+		let imageTeam = document.getElementById("image-team");
+		let removePadding = document.getElementById("remove-padding");
+		let careerCard = document.getElementById("career-card");
 		if(window.innerWidth <= 991){
-			handleClass("services-container", ["no-margin", "center", "column"], true);
-			handleClass("services-container", ["middle-container", "great-horizontal-margin"], false);
-			handleClass("services-container2", ["no-margin", "center", "column"], true);
-			handleClass("services-container2", ["middle-container", "great-horizontal-margin"], false);
-			handleClass("services-container3", ["no-margin", "center", "column"], true);
-			handleClass("services-container3", ["middle-container", "great-horizontal-margin"], false);
-			handleClass("remove-width", ["full-width"], false);
-			handleClass("remove-width", ["responsive-button-padding"], true);
-			handleClass("remove-width2", ["full-width"], false);
-			handleClass("remove-width2", ["responsive-button-padding"], true);
-			handleClass("remove-width3", ["full-width"], false);
-			handleClass("remove-width3", ["responsive-button-padding"], true);
+			if (servicesContainer && servicesContainer2 && removeWidth && removeWidth2){
+				handleClass("services-container", ["no-margin", "center", "column"], true);
+				handleClass("services-container", ["middle-container", "great-horizontal-margin"], false);
+				handleClass("services-container2", ["no-margin", "center", "column"], true);
+				handleClass("services-container2", ["middle-container", "great-horizontal-margin"], false);
+				handleClass("services-container3", ["no-margin", "center", "column"], true);
+				handleClass("services-container3", ["middle-container", "great-horizontal-margin"], false);
+				handleClass("remove-width", ["full-width"], false);
+				handleClass("remove-width", ["responsive-button-padding"], true);
+				handleClass("remove-width2", ["full-width"], false);
+				handleClass("remove-width2", ["responsive-button-padding"], true);
+				handleClass("remove-width3", ["full-width"], false);
+				handleClass("remove-width3", ["responsive-button-padding"], true);
+			}
 			handleClass("footer-coal", ["coal-black-bg"], false);
 			handleClass("footer-coal", ["valign-wrapper"], false);
-			document.getElementById("learn-about").innerHTML = "About Us";
+			if(aboutUs){
+				handleClass("about-us", ["extra-horizontal-padding"], false);
+				handleClass("about-us", ["base-horizontal-padding"], true)
+				handleClass("mision-vision", ["great-horizontal-padding"], false);
+				handleClass("history", ["great-horizontal-padding"], false);
+				handleClass("awards", ["great-horizontal-padding"], false);
+				handleClass("image-team", ["big-horizontal-padding"], false)
+			}
+			if(removePadding){
+				handleClass("remove-padding", ["extra-horizontal-padding"], false);
+			}
+			if(document.getElementById("learn-about")){
+				document.getElementById("learn-about").innerHTML = "About Us";
+			}
+			if(document.getElementById("title-form")){
+				document.getElementById("title-form").innerHTML = "Fill to apply"
+			}
+			if (careerCard){
+				handleClass("career-card", ["extra-padding"], false);
+				handleClass("career-card", ["base-horizontal-padding"], true);
+			}
 		} else {
-			document.getElementById("learn-about").text = "Learn more about us";
-			servicesContainer.classList.add("middle-container");
-			servicesContainer2.classList.add("middle-container");
-			removeWidth.classList.remove("responsive-button-padding");
-			removeWidth.classList.add("full-width");
-			removeWidth2.classList.remove("responsive-button-padding");
-			removeWidth2.classList.add("full-width");
+			handleClass("footer-coal", ["coal-black-bg"], true);
+			handleClass("footer-coal", ["valign-wrapper"], true);
+			if(document.getElementById("learn-about")){
+				document.getElementById("learn-about").text =  "Learn more about us";
+			}
+			if(document.getElementById("title-form")){
+				document.getElementById("title-form").innerHTML = "llena la tabla para Aplicar para el trabajo";
+			}
+			if(servicesContainer && servicesContainer2 && removeWidth && removeWidth2){
+				servicesContainer.classList.add("middle-container");
+				servicesContainer2.classList.add("middle-container");
+				removeWidth.classList.remove("responsive-button-padding");
+				removeWidth.classList.add("full-width");
+				removeWidth2.classList.remove("responsive-button-padding");
+				removeWidth2.classList.add("full-width");
+			}
+			if(aboutUs){
+				aboutUs.classList.remove("big-horizontal-padding");
+				aboutUs.classList.add("extra-horizontal-padding");
+				history.classList.add("great-horizontal-padding");
+				awards.classList.add("great-horizontal-padding");
+				imageTeam.classList.add("big-horizontal-padding");
+			}
+			if(removePadding){
+				removePadding.classList.add("extra-horizontal-padding");
+			}
+			if(careerCard){
+				careerCard.classList.add("extra-padding");
+			}
 		}
-		console.log(window.innerWidth);
 		/*var landscape = document.getElementById('landscape') ? document.getElementById('landscape') : null;
 		var navbar = document.getElementById('navbar');
 		var services = document.getElementById('services');
